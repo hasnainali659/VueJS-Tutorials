e1e568ec91cf227410f3031fe2602778ce43abc8
hasnainali659 <hasnainali.ha659@gmail.com>
2024-10-27 23:59:21 +0500

    vue event modifier does same as we did manually before

--- Section 2 - Events-starting-code/app.js
+++ Section 2 - Events-starting-code/app.js
@@ -16,8 +16,7 @@ const app = Vue.createApp({
     setName(event, lastName) {
       this.name = event.target.value + ' ' + lastName 
     },
-    submitForm(event) {
-      event.preventDefault();
+    submitForm() {
       alert('Submitted!');
     }
   },
